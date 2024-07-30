93a8afcaf27955b3b0173c437f967b3251080bb8
Flávia Correia <fllaviacorreia@outlook.com>
2024-07-30 08:16:31 -0300

    include comments

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ document.getElementById('taskForm').addEventListener('submit', function(e) {
     addTask();
 });
 
+// GET das Tasks salvas no Local Storage
 function loadTasks() {
     const tasks = JSON.parse(localStorage.getItem('tasks')) || [];
     tasks.forEach(task => {
@@ -11,6 +12,7 @@ function loadTasks() {
     });
 }
 
+// POST da nova task
 function saveTasks() {
     const tasks = [];
     document.querySelectorAll('#taskList li').forEach(listItem => {
@@ -21,21 +23,30 @@ function saveTasks() {
     localStorage.setItem('tasks', JSON.stringify(tasks));
 }
 
+// Mostra a nova taks na lista
 function renderTask(taskText, completed) {
+    // pega o <ul></ul>
     const taskList = document.getElementById('taskList');
+
+    // cria um novo item <li></li>
     const listItem = document.createElement('li');
 
+    // cria um novo item checkbox
     const checkbox = document.createElement('input');
     checkbox.type = 'checkbox';
     checkbox.checked = completed;
+
+    // se checkbox selecionado, tacha o item como completo e mostra pro usuário
     checkbox.addEventListener('change', function() {
         listItem.classList.toggle('completed');
         saveTasks();
     });
 
+    
     const label = document.createElement('label');
     label.textContent = taskText;
 
+    // cria o botão para deletr uma taks
     const deleteButton = document.createElement('button');
     deleteButton.textContent = 'Apagar';
     deleteButton.addEventListener('click', function() {
@@ -43,6 +54,7 @@ function renderTask(taskText, completed) {
         saveTasks();
     });
 
+    // seta no <li></li> o checkbox, o label e o botão de delete
     listItem.appendChild(checkbox);
     listItem.appendChild(label);
     listItem.appendChild(deleteButton);
@@ -51,9 +63,11 @@ function renderTask(taskText, completed) {
         listItem.classList.add('completed');
     }
 
+    // adiciona no <ul></ul> o <li></li> criado
     taskList.appendChild(listItem);
 }
 
+// verifica se o input está vazio e se não chama o renderTask(), saveTasks() e seta o input para ''
 function addTask() {
     const taskInput = document.getElementById('taskInput');
     const taskText = taskInput.value.trim();
